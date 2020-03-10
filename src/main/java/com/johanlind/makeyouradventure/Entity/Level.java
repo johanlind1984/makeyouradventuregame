03bacb1ab151d43179ef77411849006a404e9089
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-03-10 20:16:28 +0100

    Mapped Level to room
    Now a Level successfully reads in rooms from database. Room successfully reads ItemContainers, Enemies and Questions.

--- src/main/java/com/johanlind/makeyouradventure/Entity/Level.java
+++ src/main/java/com/johanlind/makeyouradventure/Entity/Level.java
@@ -2,6 +2,7 @@ package com.johanlind.makeyouradventure.Entity;
 
 import javax.persistence.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Entity
@@ -11,16 +12,41 @@ public class Level {
     @Id()
     @Column(name="id_level")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int roomId;
+    int levelId;
 
     @Column(name="level_name")
     String levelName;
 
-//    @ManyToMany(mappedBy = "level")
-//    @JoinTable(name="level_room")
-//    private Map<Integer, Room> roomList;
+    @ManyToMany(mappedBy = "levelList")
+    private List<Room> roomList;
 
     public Level() {
 
     }
+
+    public int getLevelId() {
+        return levelId;
+    }
+
+    public void setLevelId(int levelId) {
+        this.levelId = levelId;
+    }
+
+    public String getLevelName() {
+        return levelName;
+    }
+
+    public void setLevelName(String levelName) {
+        this.levelName = levelName;
+    }
+
+    public List<Room> getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(List<Room> roomList) {
+        this.roomList = roomList;
+    }
+
+
 }
